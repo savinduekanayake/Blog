fd2cfd0e0a8a374061138d49feef1fad25a6ec41
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2020-03-03 10:34:36 +0530

    add a post page

--- app.py
+++ app.py
@@ -2,9 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+all_posts = [
+    {
+        'title': 'Post 1',
+        'content': 'this is the content of post 1',
+        'author': 'Savindu'
+    },
+    {
+        'title': 'Post 2',
+        'content': 'this is the content of post 2',
+        
+    },
+]
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/posts')
+def posts():
+    return render_template('post.html',posts=all_posts)
 
 @app.route('/home/users/<string:name>/posts/<int:id>')
 def hello(name,id):
