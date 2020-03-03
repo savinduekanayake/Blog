557b8601db2ed2a7cca43243a5dd19ee6fda69f3
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2020-03-03 23:07:29 +0530

    started working with db

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(20),nullable=False, default='N/A')
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return 'Blog post '+ str(self.id)
+
+
 
 all_posts = [
     {
@@ -19,9 +36,19 @@ all_posts = [
 def home():
     return render_template('index.html')
 
-@app.route('/posts')
+@app.route('/posts', methods=['GET','POST'])
 def posts():
-    return render_template('post.html',posts=all_posts)
+
+    if request.method == 'POST':
+        post_title= request.form['title']
+        post_content = request.form['content']
+        new_post = BlogPost(title=post_title,content=post_content, author='Savindu')
+        db.session.add(new_post)    #added to session
+        db.session.commit()         #added to db
+        return redirect('/posts')
+    else:
+        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
+        return render_template('post.html',posts=all_posts)
 
 @app.route('/home/users/<string:name>/posts/<int:id>')
 def hello(name,id):
