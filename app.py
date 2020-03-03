53fa77ba52343445fcb5e29647d94fee1173a8d6
Savindu Kalsara Ekanayake <47809365+savinduekanayake@users.noreply.github.com>
2020-03-03 09:52:55 +0530

    render using templates

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Home page</h1>"
+    return render_template('index.html')
 
 @app.route('/home/users/<string:name>/posts/<int:id>')
 def hello(name,id):
